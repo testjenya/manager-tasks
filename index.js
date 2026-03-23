e66b80c0466432f65bebfb0f1d28bfeeb05c4b58
Jane <jane@example.com>
2026-03-23 14:43:50 +0300

    менеджер задач 3

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const taskArray = []
+let taskArray = []
 let completedTaskCount = 0
 let IdCount = 1
 
-function addTask(title, description) {
+function addTask(title, description, createdDate) {
   const task = {
     id: IdCount++,
     title,
     description,
     isCompleted: false,
-    createdDate: new Date(),
+    createdDate,
     completedDate: null
   }
   taskArray.push(task)
 }
 
-addTask('Купить продукты', 'Купить хлеб')
-addTask('Купить продукты1', 'Купить хлеб1')
-addTask('Купить продукты2', 'Купить хлеб2')
-
+addTask('Купить продукты', 'Купить хлеб', new Date('2026-03-20T09:00:00'))
+addTask('Купить продукты1', 'Купить хлеб1', new Date('2026-03-17T09:05:00'))
+addTask('Купить продукты2', 'Купить хлеб2', new Date('2026-03-10T09:10:00'))
 
 
 function completeTask(id) {
@@ -31,7 +30,7 @@ function completeTask(id) {
 }
 completeTask(2)
 
-console.log('Выполнено задач:', completedTaskCount)
+// console.log('Выполнено задач:', completedTaskCount)
 
 
 
@@ -45,7 +44,7 @@ function deleteTask(id) {
     console.log('Задача не найдена')
   }
 }
-deleteTask(2)
+// deleteTask(2)
 
 
 
@@ -67,6 +66,72 @@ function clearTasks() {
   }
 }
 
-clearTasks(taskArray)
+// clearTasks(taskArray)
 
-console.log(taskArray)
+// console.log(taskArray)
+
+
+
+
+
+
+
+// ================== Менеджер задач 3 ========================
+// 1
+function getTaskDescriptions() {
+  return taskArray.map(task => task.description)
+}
+// console.log('массив description задач:', getTaskDescriptions())
+// console.log(taskArray)
+
+
+// 2
+function getLongTasks() {
+  return taskArray.filter(task => task.title.length > 10)
+}
+// console.log('длина title более 10 символов:', getLongTasks())
+// console.log(taskArray)
+
+
+// 3
+function getTasksByDateRange(startDate, endDate, isCompleted = false) {
+  return taskArray.filter(function (task) {
+    // console.log(task.createdDate)
+
+    if (task.isCompleted === true) {
+      return task
+    }
+  })
+}
+// console.log(getTasksByDateRange(new Date('2026-03-20T09:15:00'), new Date('2026-03-20T09:15:00')))
+// console.log(taskArray)
+
+
+// 4
+function clearShortTasks(arr) {
+  return arr.filter(task => task.title.length >= 5)
+}
+taskArray = clearShortTasks(taskArray)
+
+// console.log('удалить задачи длиной менее 5 символов', clearShortTasks())
+// console.log(taskArray)
+
+
+// 5
+function changeTaskTitle(index, newTitle) {
+  return taskArray[index].title = newTitle
+}
+// console.log('изменить title задачи:', changeTaskTitle(0, 'Новое Имя'))
+// console.log(taskArray)
+
+
+// 6 completeTask. уже реализовали в строке 22
+
+
+// 7
+function showTasks() {
+  taskArray.forEach(task => console.log(task))
+}
+// console.log('свойства задач:');
+// showTasks();
+
